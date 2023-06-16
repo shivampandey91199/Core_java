52243f44e880def0578071e06eb465735b62b0b4
Pandey Shivam <111884088+shivampandey91199@users.noreply.github.com>
2023-06-16 17:47:09 +0530

    Add files via upload
    Assignment Questions

---
+++ core_java/src/Assignment/Assignment_Q18.java
@@ -0,0 +1,12 @@
+
+/*  W.A.J.P to find the second most frequent character in a given string.
+ *   The given string is: successes 
+ *   The second most frequent char in the string is: c  */
+package Assignment;
+
+public class Assignment_Q18 {
+   public static void main(String[] args) {
+	String st="successes";
+	System.out.println(st.repeat(2));
+}
+}
